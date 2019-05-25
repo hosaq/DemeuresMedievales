8cab1b16a3e52485d340056f84b4fbe800ac85e2
ortega <ortegagilles@orange.fr>
2019-05-25 16:49:08 +0200

    premier commit page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller{
+    /**
+     * Undocumented function
+     *
+     * @Route("/",name="homepage")
+     */
+   public function home(){
+       return $this->render(
+           'home.html.twig'
+       );
+
+   } 
+}
+?>
